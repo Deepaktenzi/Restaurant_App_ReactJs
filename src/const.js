23068685ae531e9ee565a5014dae73eaaccae6ee
Deepak Negi <deepaktenzi@gmail.com>
2023-03-14 16:52:28 +0530

    Mobile issue resolved

--- src/const.js
+++ src/const.js
@@ -2,10 +2,10 @@ export const IMG_CDN =
   'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/';
 
 export const FETCH_MENU_URL =
-  'https://www.swiggy.com/dapi/menu/v4/full?lat=30.322232&lng=78.085605&menuId=';
+  'https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/v4/full?lat=30.322232&lng=78.085605&menuId=';
 
 export const FETCH_RESTAURANTS_URL =
-  'https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.322232&lng=78.085605&page_type=DESKTOP_WEB_LISTING';
+  'https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.322232&lng=78.085605&page_type=DESKTOP_WEB_LISTING';
 
 export const RESTAURANT_CAROUSELS_API =
   'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/';
