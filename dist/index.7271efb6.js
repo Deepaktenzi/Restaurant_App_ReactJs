f725cf40b95d4c98cb8c170700dc1ef689ec0a9a
Deepak Negi <deepaktenzi@gmail.com>
2023-04-15 14:29:17 +0530

    Code Updated

--- dist/index.7271efb6.js
+++ dist/index.7271efb6.js
@@ -26940,7 +26940,7 @@ exports.RESTAURANT_CAROUSELS_API = exports.IMG_CDN = exports.FETCH_RESTAURANTS_U
 const IMG_CDN = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 // https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/
 exports.IMG_CDN = IMG_CDN;
-const FETCH_MENU_URL = "http://localhost:4000/api/menu?id=";
+const FETCH_MENU_URL = "https://api-94b2.onrender.com/api/menu?id=";
 // 'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.322232&lng=78.085605&restaurantId=';
 exports.FETCH_MENU_URL = FETCH_MENU_URL;
 const FETCH_RESTAURANTS_URL = "https://api-94b2.onrender.com/api/restaurants?lat=30.322232&lng=78.085605";
