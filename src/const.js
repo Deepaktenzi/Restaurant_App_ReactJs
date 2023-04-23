b40be5ee7ae82d906431c318dea8d73f03704311
Deepak Negi <deepaktenzi@gmail.com>
2023-04-23 11:13:21 +0530

    New Backend

--- src/const.js
+++ src/const.js
@@ -3,10 +3,10 @@ export const IMG_CDN =
 
 // https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/
 
-export const FETCH_MENU_URL = 'https://api-94b2.onrender.com/api/menu?id=';
+export const FETCH_MENU_URL = 'https://foodishhub.live/api/menu?id=';
 // 'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.322232&lng=78.085605&restaurantId=';
 export const FETCH_RESTAURANTS_URL =
-  'https://api-94b2.onrender.com/api/restaurants?lat=30.322232&lng=78.085605';
+  'https://foodishhub.live/api/restaurants?lat=30.322232&lng=78.085605';
 
 export const RESTAURANT_CAROUSELS_API =
   'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/';
